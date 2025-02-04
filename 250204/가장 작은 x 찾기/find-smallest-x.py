b0511889457abd162aa2d671c89d98f47ca4847b
Sehee An <49268298+seheeAn@users.noreply.github.com>
2025-02-04 22:07:11 +0900

    Title: 가장 작은 x 찾기, Time: 78ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 250204/가장 작은 x 찾기/find-smallest-x.py
+++ 250204/가장 작은 x 찾기/find-smallest-x.py
@@ -11,6 +11,30 @@ for i in range(1, 11):
     for j in range(n):
         if num * 2**(j+1) >= a[j] and num *2**(j+1) <= b[j]:
             flag = True
+    for j in range(n):
+        if num * 2**(j+1) >= a[j] and num *2**(j+1) <= b[j]:
+            flag = True
+        else:
+            flag = False
+            break
+    
+    if flag == True:
+        print(i)
+        break
+
+테스트케이스
+
+
+결과
+
+
+코드 실행
+
+
+제출 및 채점
+
+
+
         else:
             flag = False
             break
