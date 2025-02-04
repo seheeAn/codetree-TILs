487de37d1b41f7d09f970ded432333202d251984
Sehee An <49268298+seheeAn@users.noreply.github.com>
2025-02-04 22:06:35 +0900

    Title: 가장 작은 x 찾기, Time: 43ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250204/가장 작은 x 찾기/find-smallest-x.py
@@ -0,0 +1,20 @@
+n = int(input())
+ranges = [tuple(map(int, input().split())) for _ in range(n)]
+a, b = zip(*ranges)
+a, b = list(a), list(b)
+
+# Write your code here!
+for i in range(1, 11):
+    flag = False
+    num = i
+
+    for j in range(n):
+        if num * 2**(j+1) >= a[j] and num *2**(j+1) <= b[j]:
+            flag = True
+        else:
+            flag = False
+            break
+    
+    if flag == True:
+        print(i)
+        break
